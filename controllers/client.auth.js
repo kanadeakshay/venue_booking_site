9609bca5d14ad7bae6a95d15518a73b152f9fa94
akshaykanade <akshayk0782@gmail.com>
2021-08-23 00:14:44 +0530

    :passport_control: add: controllers for auth routes

---
+++ controllers/client.auth.js
@@ -0,0 +1,53 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const signup = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .exec((error, user) => {
+            // If user already exists
+            if (error) return res.status(400).json({ msg: `Something went wrong`, error });
+            if (user) return res.status(400).json({ msg: 'User already exits' });
+
+            // If new user trys to login
+            const { firstName, lastName, email, password, contactNumber } = req.body;
+            const _user = new User({
+                firstName, lastName, email, password, contactNumber,
+                username: Math.random().toString()
+            })
+
+            _user.save((error, data) => {
+                if (error) return res.status(400).json({ msg: `Something went wrong`, error });
+                if (data) return res.status(201).json({ msg: 'User Successfully register !!' });
+            })
+        })
+}
+
+const signin = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .exec((error, user) => {
+            if (error) return res.status(400).json({ msg: `Something went wrong`, error });
+            if (user) {
+                if (user.authenticate(req.body.password) && user.role === 'client') {
+                    const token = jwt.sign(
+                        { id: user._id, role: user.role },
+                        process.env.jwt_secret,
+                        { expiresIn: '2h' }
+                    )
+                    const { _id, firstName, lastName, email, role, fullName, username, contactNumber } = user;
+                    res.status(200).json({
+                        token,
+                        user: {
+                            _id, firstName, lastName, email, role, fullName, username, contactNumber
+                        }
+                    })
+                } else {
+                    return res.status(400).json({ msg: `Invalid Password` })
+                }
+            }
+        })
+}
+
+module.exports = {
+    signup,
+    signin
+}
