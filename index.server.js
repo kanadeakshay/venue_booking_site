7f557bbc09fb3435f016667345aca69a1d80c439
akshaykanade <akshayk0782@gmail.com>
2021-08-23 20:59:01 +0530

    :recycle: update: make database connection async

--- index.server.js
+++ index.server.js
@@ -3,30 +3,42 @@ const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
 
+// enviourment variables
+env.config();
+
+//middlewares
+app.use(express.json());
+
 // Routes
 const dealerAuthRoutes = require('./routes/dealer.auth');
 const clientAuthRoutes = require('./routes/client.auth');
 // const venueRoutes = require('./routes/venue');
 
-// enviourment variables
-env.config();
-
-// mongodb connection
-mongoose.connect(process.env.dburl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-}).then(() => {
-    console.log('Database Connected');
-})
-
-app.use(express.json());
-
 app.use('/api', dealerAuthRoutes);
 app.use('/api', clientAuthRoutes);
 // app.use('/api', venueRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+// mongodb connection
+const connectDB = (dburl) => {
+    return mongoose.connect(dburl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    }).then(() => {
+        console.log('Database Connected');
+    })
+}
+
+const start = async () => {
+    try {
+        await connectDB(process.env.dburl);
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
