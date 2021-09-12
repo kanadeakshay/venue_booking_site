e57fc87ca2e57707e015f74d1a691f71121e0158
akshaykanade <akshayk0782@gmail.com>
2021-09-12 22:32:07 +0530

    :bridge_at_night: update: add redux-store to app

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+window.store = store;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
