7875637798e6510163d7c6c7e6b7459ab65c01de
akshaykanade <akshayk0782@gmail.com>
2021-09-12 21:48:48 +0530

    :truck: add: deals routes and cors middleware

--- index.server.js
+++ index.server.js
@@ -3,22 +3,26 @@ const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 // enviourment variables
 env.config();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routes
 const dealerAuthRoutes = require('./routes/dealer.auth');
 const clientAuthRoutes = require('./routes/client.auth');
 const venueRoutes = require('./routes/venue');
+const dealsRoutes = require('./routes/deal');
 
 app.use("/public", express.static(path.join(__dirname, "uploads")));
 app.use('/api', dealerAuthRoutes);
 app.use('/api', clientAuthRoutes);
 app.use('/api', venueRoutes);
+app.use('/api', dealsRoutes);
 
 // mongodb connection
 const connectDB = (dburl) => {
