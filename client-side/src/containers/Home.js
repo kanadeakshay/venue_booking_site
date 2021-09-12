cbdc03e53e45d00d15169e82ada633126270c7cd
akshaykanade <akshayk0782@gmail.com>
2021-09-12 23:03:01 +0530

    :construction: add: home page :house_with_garden:

---
+++ client-side/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../components/Layout/index.layout';
+import { Container } from 'react-bootstrap';
+
+function Home() {
+    return (
+        <Layout>
+            <Container>
+                <h1>Home page 🏡</h1>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Home
