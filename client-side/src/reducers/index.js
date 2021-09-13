1563c7e284baf8df1aad50a19e10c21f0c60c54c
akshaykanade <akshayk0782@gmail.com>
2021-09-14 01:08:02 +0530

    :globe_with_meridians: update: combine new reducer

--- client-side/src/reducers/index.js
+++ client-side/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth.reducers'
 import registerReducer from './register.reducers';
+import userInfoReducer from './userInfo.reducers';
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    regStats: registerReducer
+    registrationStatus: registerReducer,
+    userInfo: userInfoReducer
 });
 
 export default rootReducer
