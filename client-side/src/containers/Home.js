4574b5d28c92a8ccb5325321cb472cc2649f0ef7
akshaykanade <akshayk0782@gmail.com>
2021-09-15 12:40:46 +0530

    :lipstick: update: render venues and user info on home and profile page

--- client-side/src/containers/Home.js
+++ client-side/src/containers/Home.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout/index.layout';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
+import VenueCard from '../components/UI/VenueCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { getVenues } from '../actions/venue.actions';
+import { getPublicURL } from '../urlConfig';
 
 function Home() {
+
+    const allVenuesInfo = useSelector(state => state.allVenuesInfo);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getVenues());
+    }, [])
+
+
+    if (allVenuesInfo.loading) {
+        return (
+            <Layout>
+                <div className='text-center' style={{ marginTop: '60px' }}>
+                    <h1>Getting all venues 🎉</h1>
+                    <Spinner animation="border" variant="success" />
+                </div>
+            </Layout>
+        );
+    }
+
     return (
         <Layout>
             <Container>
-                <h1>Home page 🏡</h1>
+                <div className="row" style={{ marginTop: "30px" }}>
+                    {
+                        allVenuesInfo.allVenues.map((venue) => {
+                            const { venueName, address, location, category, price, venuePictures } = venue;
+                            return (
+                                <div className="col-md-4">
+                                    <VenueCard
+                                        img1={venuePictures[0].img}
+                                        img2={venuePictures[1].img}
+                                        venueName={venueName}
+                                        category={category}
+                                        address={address}
+                                        location={location}
+                                        price={price}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </Container>
         </Layout>
     )
--- client-side/src/containers/Profile.js
+++ client-side/src/containers/Profile.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Container, Spinner, Card, Button } from 'react-bootstrap';
+import { Container, Spinner, Button } from 'react-bootstrap';
 import Layout from '../components/Layout/index.layout';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
+import VenueCard from '../components/UI/VenueCard';
+import { ProfileCard, UserInfoCard } from '../components/UI/ProfileCards';
+import { ClientHistory } from '../components/UI/ClientHistory';
 
 const ProfilePage = (props) => {
 
@@ -29,139 +32,45 @@ const ProfilePage = (props) => {
     return (
         <Layout>
             <Container>
-                <div className="container">
-                    <div className="main-body" style={{ marginTop: '30px' }}>
-
-                        <div className="row gutters-sm">
-                            <div className="col-md-4 mb-3">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <div className="d-flex flex-column align-items-center text-center">
-                                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="User" className="rounded-circle" style={{ width: '100px' }} />
-                                            <div className="mt-3">
-                                                <h4 style={{ textTransform: 'capitalize' }}>{fullName}</h4>
-                                                <p className="text-secondary mb-1">Email - {email}</p>
-                                                <p className="text-secondary mb-1">Contact no - {contactNumber}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="card mt-3">
-                                    <ul className="list-group list-group-flush">
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0">User Type</h6>
-                                            <span className="text-secondary" style={{ textTransform: 'capitalize' }}>{role}</span>
-                                        </li>
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0">Username</h6>
-                                            <span className="text-secondary">{username}</span>
-                                        </li>
-                                        <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                            <h6 className="mb-0">User since</h6>
-                                            <span className="text-secondary">{createdAt}</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-
-                            <div className="col-md-8">
-                                <div className="card mb-3">
-                                    <div className="card-body">
-                                        <h4>Last few bookings</h4>
-                                        <hr style={{ width: '30%' }} />
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <h6 className="mb-0">User Type</h6>
-                                            </div>
-                                            <div className="col-sm-9 text-secondary" style={{ textTransform: 'capitalize' }}>
-                                                {role}
-                                            </div>
-                                        </div>
-                                        <hr></hr>
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <h6 className="mb-0">Full Name</h6>
-                                            </div>
-                                            <div className="col-sm-9 text-secondary" style={{ textTransform: 'capitalize' }}>
-                                                {fullName}
-                                            </div>
-                                        </div>
-                                        <hr></hr>
-                                        <div className="row">
-                                            <div className="col-sm-3">
-                                                <h6 className="mb-0">Username</h6>
-                                            </div>
-                                            <div className="col-sm-9 text-secondary">
-                                                {username}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                {/* <div class="row gutters-sm">
-                                    <div class="col-sm-6 mb-3">
-                                        <div class="card h-100">
-                                            <div class="card-body">
-                                                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">assignment</i>Project Status</h6>
-                                                <small>Web Design</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>Website Markup</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>One Page</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>Mobile Template</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>Backend API</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6 mb-3">
-                                        <div class="card h-100">
-                                            <div class="card-body">
-                                                <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">assignment</i>Project Status</h6>
-                                                <small>Web Design</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>Website Markup</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>One Page</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>Mobile Template</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                                <small>Backend API</small>
-                                                <div class="progress mb-3" style="height: 5px">
-                                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 66%" aria-valuenow="66" aria-valuemin="0" aria-valuemax="100"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> */}
-
-
-
-                            </div>
+                <div className="main-body" >
+                    <div className="row gutters-sm" style={{ marginTop: '30px' }}>
+                        <div className="col-md-4 mb-3">
+                            <ProfileCard
+                                fullName={fullName}
+                                email={email}
+                                contactNumber={contactNumber}
+                            />
+                            <UserInfoCard
+                                role={role}
+                                username={username}
+                                createdAt={createdAt}
+                            />
                         </div>
 
+                        <div className="col-md-8">
+                            <div className="card mb-3">
+                                <ClientHistory />
+                            </div>
+                            {
+                                userInfo.user.role === 'dealer' ?
+                                    <>
+                                        <Button variant="success">+ New Venue</Button>
+                                        <hr></hr>
+                                        <div className="row gutters-sm">
+                                            <h4 style={{ marginBottom: "15px" }}>Your All venues</h4>
+                                            <div className="col-sm-6 mb-3">
+                                                <VenueCard />
+                                            </div>
+                                            <div className="col-sm-6 mb-3">
+                                                <VenueCard />
+                                            </div>
+                                        </div>
+                                    </>
+                                    : <></>
+                            }
+                        </div>
                     </div>
+
                 </div>
             </Container >
         </Layout >
