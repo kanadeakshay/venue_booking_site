5f806994dfec69549beb1458242bb903966e7cc9
akshaykanade <akshayk0782@gmail.com>
2021-09-16 23:13:26 +0530

    :tada: update: add new actions in venue file

--- client-side/src/actions/venue.actions.js
+++ client-side/src/actions/venue.actions.js
@@ -1,5 +1,30 @@
-import { venueConstants } from "./constants";
+import { venueConstants, addVenueConstants } from "./constants";
 import axios from '../helpers/axios';
+
+const addVenue = (form) => {
+    return async (dispatch) => {
+        dispatch({
+            type: addVenueConstants.ADD_VENUE_REQUEST
+        });
+
+        const res = await axios.post(`/create-venue`, form);
+
+        if (res.status === 201) {
+            dispatch({
+                type: addVenueConstants.ADD_VENUE_SUCCESS,
+                payload: res.data._venue
+            });
+        } else {
+            dispatch({
+                type: addVenueConstants.ADD_VENUE_FAILURE,
+                payload: {
+                    msg: res.data.msg,
+                    error: res.data.error
+                }
+            });
+        }
+    }
+}
 
 const getVenues = () => {
     return async (dispatch) => {
@@ -25,6 +50,60 @@ const getVenues = () => {
     }
 }
 
+const getOneVenue = (id) => {
+    return async (dispatch) => {
+        dispatch({
+            type: venueConstants.GETONE_VENUE_REQUEST
+        });
+
+        const res = await axios.get(`/venue/${id}`);
+
+        if (res.status === 200) {
+            dispatch({
+                type: venueConstants.GETONE_VENUE_SUCCESS,
+                payload: {
+                    venue: res.data._venue
+                }
+            });
+        } else {
+            dispatch({
+                type: venueConstants.GETONE_VENUE_FAILURE,
+                payload: {
+                    msg: res.data.msg
+                }
+            });
+        }
+    }
+}
+
+const getOwnerVenues = (ownerId) => {
+    return async (dispatch) => {
+        dispatch({
+            type: venueConstants.GETALL_VENUES_OF_DEALER_REQUEST
+        });
+
+        const res = await axios.get(`/venues/${ownerId}`);
+
+        if (res.status === 200) {
+            dispatch({
+                type: venueConstants.GETALL_VENUES_OF_DEALER_SUCCESS,
+                payload: res.data._allvenues
+            });
+        } else {
+            dispatch({
+                type: venueConstants.GETALL_VENUES_OF_DEALER_FAILURE,
+                payload: {
+                    msg: res.data.msg,
+                    error: res.data.error
+                }
+            });
+        }
+    }
+}
+
 export {
-    getVenues
+    addVenue,
+    getVenues,
+    getOneVenue,
+    getOwnerVenues
 }
