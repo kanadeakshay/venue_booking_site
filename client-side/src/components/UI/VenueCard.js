e11aee8299f56b094426a0d327ec4186e852d8cc
akshaykanade <akshayk0782@gmail.com>
2021-09-16 21:57:13 +0530

    :lipstick: update: show/hide 'Book' button

--- client-side/src/components/UI/VenueCard.js
+++ client-side/src/components/UI/VenueCard.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import img1 from '../../assets/images/client-signin.png';
-import img2 from '../../assets/images/dealer-signin.png';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { getOneVenue } from '../../actions/venue.actions';
 import { getPublicURL } from '../../urlConfig';
 import { ImgsCard } from './ImgsCard';
+import { useDispatch } from 'react-redux';
 
 const VenueCard = (props) => {
-    const { img1, img2, category, venueName, price, location, address } = props;
+
+    const { img1, img2, category, venueName, _id, price, location, address, style, isHide } = props;
+
+    const dispatch = useDispatch()
+    const getVenueInfo = () => {
+        dispatch(getOneVenue(_id));
+    }
+
     return (
         <div className="card mb-4 box-shadow">
             <ImgsCard
                 img1={getPublicURL(img1)}
                 img2={getPublicURL(img2)}
                 alt='venue picture'
+                style={style}
             />
             <div className="card-body">
                 <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
@@ -24,41 +32,20 @@ const VenueCard = (props) => {
                 <h6 className="card-subtitle mb-2 text-muted">{location}, {address}</h6>
 
                 <div className="d-flex justify-content-between align-items-center">
-                    <NavLink to="venue" className="btn-group">
-                        <Button variant="primary">Details</Button>{' '}
-                    </NavLink>
-                    <Button variant="danger">Book</Button>
+                    <Link to={`/venue/${_id}`} className="btn-group">
+                        <Button variant="primary" onClick={getVenueInfo}>Details</Button>{' '}
+                    </Link>
+                    {
+                        isHide === true ?
+                            <></>
+                            :
+                            <Button variant="danger">Book</Button>
+                    }
 
                 </div>
             </div>
         </div>
     )
-}
-
-
-const VenueCardV1 = () => {
-    return (
-        <Card style={{ width: '18rem' }}>
-            <ImgsCard
-                img1={img1}
-                img2={img2}
-                alt='venue picture'
-            />
-            <Card.Body>
-                <Card.Subtitle className="mb-2 text-muted">Category</Card.Subtitle>
-                <div className="d-flex justify-content-between align-items-center">
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Title>Price</Card.Title>
-                </div>
-                <Card.Subtitle className="mb-2 text-muted">Location, Address</Card.Subtitle>
-
-                <NavLink to="venue">
-                    <Button variant="primary" size="sm">Details</Button>{' '}
-                </NavLink>
-                <Button variant="danger" size="sm">Book</Button>
-            </Card.Body>
-        </Card>
-    );
 }
 
 export default VenueCard
