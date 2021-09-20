64e1917e26fddde5bce3abb5dbe90655ab1c6dd5
akshaykanade <akshayk0782@gmail.com>
2021-09-20 17:12:00 +0530

    :wrench: update: Input model

--- client-side/src/components/UI/Input.js
+++ client-side/src/components/UI/Input.js
@@ -5,13 +5,24 @@ const Input = (props) => {
     return (
         <Form.Group className="mb-3">
             <Form.Label>{props.label}</Form.Label>
-            <Form.Control
-                type={props.type}
-                placeholder={props.placeholder}
-                value={props.value}
-                onChange={props.onChange}
-                required
-            />
+            {props.isReadOnly ?
+                <Form.Control
+                    type={props.type}
+                    placeholder={props.placeholder}
+                    value={props.value}
+                    onChange={props.onChange}
+                    required
+                    readOnly
+                />
+                :
+                <Form.Control
+                    type={props.type}
+                    placeholder={props.placeholder}
+                    value={props.value}
+                    onChange={props.onChange}
+                    required
+                />
+            }
             <Form.Text className="text-muted">
                 {props.errorMessage}
             </Form.Text>
