a1f4021d5496ccb32aed1cc28a73e1ca676ed6ef
akshaykanade <akshayk0782@gmail.com>
2021-09-25 20:01:10 +0530

    :truck: fix: auth & add: controllers for same

--- routes/deal.js
+++ routes/deal.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getDeal, confirmDealsOfUser, confirmDealsOfDealer } = require('../controllers/deal');
+const { getDeal, confirmDealsOfUser, confirmDealsOfDealer, checkout } = require('../controllers/deal');
+const { requireSignIn, clientMiddleware, dealerMiddleware } = require('../common_middlewares/index')
 const router = express.Router();
 
-router.post('/checkout/:userId', () => { });
-router.get('/confirm-deals/:userId', confirmDealsOfUser);
-router.get('/confirm-deals/:dealerId', confirmDealsOfDealer);
-router.get('/deal/:dealId', getDeal)
+router.post('/checkout', requireSignIn, checkout);
+router.get('/confirm-deals/:userId', requireSignIn, clientMiddleware, confirmDealsOfUser);
+router.get('/confirm-deals-dealer/:dealerId', requireSignIn, dealerMiddleware, confirmDealsOfDealer);
+router.get('/deal/:dealId', requireSignIn, getDeal);
 
 module.exports = router;
