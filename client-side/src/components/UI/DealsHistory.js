c33d6fe68f4db3b001326d3a8f32614dcdacf634
akshaykanade <akshayk0782@gmail.com>
2021-09-25 23:14:46 +0530

    :lipstick: update: Deals history table model

--- client-side/src/components/UI/DealsHistory.js
+++ client-side/src/components/UI/DealsHistory.js
@@ -1,39 +1,56 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
+import { isEmpty } from '../../helpers/isObjEmpty';
 
 const DealsHistory = (props) => {
     return (
         <div className="card-body">
             <h4>Last few bookings</h4>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Deal Date</th>
-                        <th>Venue Name</th>
+            {
+                isEmpty(props.allDeals) ?
+                    <h5>
                         {
-                            props.role === 'dealer' ?
-                                <th>Per deal revenue</th> : <th>Bill per deal</th>
+                            props.role === 'client' ?
+                                `You didn't booked any venues`
+                                :
+                                `Currently you don't have any bookings`
                         }
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>09/09/2021</td>
-                        <td>AV Banquet hall</td>
-                        <td>₹ 7000</td>
-                    </tr>
-                    <tr>
-                        <td>10/09/2021</td>
-                        <td>JV Mirrage Hall</td>
-                        <td>₹ 30000</td>
-                    </tr>
-                    {/* <tr>
-                        <td>3</td>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr> */}
-                </tbody>
-            </Table>
+                    </h5>
+                    :
+                    <Table striped hover>
+                        <thead>
+                            <tr>
+                                <th>Deal Date</th>
+                                <th>Venue Name</th>
+                                <th>Event Date</th>
+                                {
+                                    props.role === 'dealer' ?
+                                        <th>Per deal revenue</th> : <th>Bill per deal</th>
+                                }
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                props.allDeals.map((deal) => {
+                                    const { date_added, venueName, eventDate, bill } = deal;
+                                    return (
+                                        <tr>
+                                            <td>{date_added}</td>
+                                            <td>{venueName}</td>
+                                            <td>{eventDate}</td>
+                                            <td>{bill}</td>
+                                            <td>
+                                                <Button size="sm">Details</Button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </Table>
+            }
+
         </div>
     )
 }
