a9724b752d448c1fefe427d08663ccca8a3051ab
akshaykanade <akshayk0782@gmail.com>
2021-09-29 00:26:07 +0530

    :truck: add: new routes for deals

--- routes/deal.js
+++ routes/deal.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getDeal, confirmDealsOfUser, confirmDealsOfDealer, checkout } = require('../controllers/deal');
+const { getDeal, confirmDealsOfUser, confirmDealsOfDealer, checkout, confirmDeal, deleteUnconfirmDeal } = require('../controllers/deal');
 const { requireSignIn, clientMiddleware, dealerMiddleware } = require('../common_middlewares/index')
 const router = express.Router();
 
 router.post('/checkout', requireSignIn, checkout);
+router.patch('/confirm-deal/:dealId', requireSignIn, confirmDeal);
+router.delete('/delete-unconfirmDeal/:dealId', requireSignIn, deleteUnconfirmDeal);
 router.get('/confirm-deals/:userId', requireSignIn, clientMiddleware, confirmDealsOfUser);
 router.get('/confirm-deals-dealer/:dealerId', requireSignIn, dealerMiddleware, confirmDealsOfDealer);
 router.get('/deal/:dealId', requireSignIn, getDeal);
