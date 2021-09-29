27c136401af747cd20ab16d2146e755ca21aca43
akshaykanade <akshayk0782@gmail.com>
2021-09-29 12:48:24 +0530

    :wrench: update: add payment/checkout function

--- client-side/src/components/UI/BookingModel.js
+++ client-side/src/components/UI/BookingModel.js
@@ -1,8 +1,40 @@
-import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Modal, Button, Form, Row, Col, Spinner } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import Input from './Input';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from '../../helpers/axios';
 
 const BookingModel = (props) => {
-    const { _id, venueName, price, category, location, address } = props;
+
+    const { _id, venueName, price, category, location, address, ownerId } = props;
+    const [date, setDate] = useState(new Date());
+    const [isLoading, setIsLoading] = useState(false);
+
+    const auth = useSelector(state => state.auth);
+
+    const gotoCheckoutPage = async (e) => {
+        if (!auth.authenticate) {
+            return <Redirect to={'/signin'} />
+        }
+        else {
+            e.preventDefault();
+            setIsLoading(true);
+            const dealInfo = {
+                venueId: _id,
+                venueName: venueName,
+                venueOwnerId: ownerId,
+                bill: price,
+                eventDate: date.toString()
+            }
+            console.log(dealInfo);
+            const res = await axios.post(`/checkout`, dealInfo);
+            localStorage.setItem('dealId', JSON.stringify(res.data.dealId));
+            window.location.href = res.data.url;
+        }
+    }
+
     return (
         <Modal
             {...props}
@@ -17,12 +49,21 @@ const BookingModel = (props) => {
                 <Button onClick={props.onHide}>X</Button>
             </Modal.Header>
             <Modal.Body>
-                <Form>
-                    <Input
-                        label='Select the Date for Event'
-                        type='date'
-                    />
+                <h5>
+                    <span style={{ color: 'red' }}><strong>Note: </strong></span>
+                    Before booking always contact owner
+                </h5>
+
+                <Form onSubmit={gotoCheckoutPage}>
                     <Row>
+                        <Col md={6}>
+                            <Input
+                                label='Select the Date for Event'
+                                type='date'
+                                value={date}
+                                onChange={(e) => setDate(e.target.value)}
+                            />
+                        </Col>
                         <Col md={6}>
                             <Input
                                 label='Venue Name'
@@ -31,6 +72,8 @@ const BookingModel = (props) => {
                                 isReadOnly={true}
                             />
                         </Col>
+                    </Row>
+                    <Row>
                         <Col md={6}>
                             <Input
                                 label='Category'
@@ -39,8 +82,6 @@ const BookingModel = (props) => {
                                 isReadOnly={true}
                             />
                         </Col>
-                    </Row>
-                    <Row>
                         <Col md={6}>
                             <Input
                                 label='Location'
@@ -49,6 +90,8 @@ const BookingModel = (props) => {
                                 isReadOnly={true}
                             />
                         </Col>
+                    </Row>
+                    <Row>
                         <Col md={6}>
                             <Input
                                 label='Address'
@@ -57,15 +100,30 @@ const BookingModel = (props) => {
                                 isReadOnly={true}
                             />
                         </Col>
+                        <Col md={6}>
+                            <Input
+                                label='Bill'
+                                type='number'
+                                value={price}
+                                isReadOnly={true}
+                                message='With Service tax included in Bill'
+                            />
+                        </Col>
                     </Row>
-                    <Input
-                        label='Bill'
-                        type='number'
-                        value={price}
-                        isReadOnly={true}
-                    />
+
                     <div className="text-center">
-                        <Button variant="success">Payment</Button>
+                        <Button variant="success" type="submit">
+                            {
+                                isLoading ?
+                                    <>
+                                        <Spinner animation="border" as="span" size="sm" variant="light" />
+                                        {" "} Processing...
+                                    </>
+                                    :
+                                    <span>Payment</span>
+                            }
+                        </Button>
+
                     </div>
                 </Form>
             </Modal.Body>
